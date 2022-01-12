7ba9ea46a46abfd76aad66a5f60be1fc6e5b8e5b
Angel Uzunov <a.svetozarov@hotmail.com>
2022-01-12 10:50:56 +0200

    3rd try

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
 import "../scss/app.scss";
 
+var prices = document.querySelectorAll(".hot");
+  console.log(prices);
+  prices.forEach((price) => {
+    let val = price.innerHTML;
+    console.log(val);
+    price.innerHTML = val + "&#128293";
+  });
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
